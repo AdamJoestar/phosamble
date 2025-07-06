29f0851625d328821e68a46c942f0997a4cf6132
AdamJoestar <adamschoolauto@gmail.com>
2025-07-06 14:10:06 +0700

    aman boy kurang collab

--- app/Models/Memory.php
+++ app/Models/Memory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Memory extends Model
 {
+    protected $fillable = ['user_id', 'type', 'title', 'content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('') }}">
+                    <a href="{{ route('boards.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-200" />
                     </a>
                 </div>
@@ -67,8 +67,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-gray-200">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('boards.index')" :active="request()->routeIs('boards.index')" class="text-gray-200">
+                {{ __('My Boards') }}
             </x-responsive-nav-link>
         </div>
 
